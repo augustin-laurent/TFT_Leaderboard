2fa9ab358d2768dc970507d3f585faa993e194f7
Augustin Laurent <augustin.lrt2@icloud.com>
2024-08-08 20:28:35 +0200

    Change style, make components out of page elements

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,34 @@
+
 import { UserButton, SignInButton, SignedIn, SignedOut } from '@clerk/nextjs';
 
 import { Button } from "@/components/ui/button";
 
+import Image from "next/image";
+
 export function Header() {
     return (
-        <header className="flex items-center justify-center w-full p-4">
-            <div className="flex-grow text-center">
-                <h1 className="text-2xl font-bold">TFT Leaderboard</h1>
+        <header className="flex flex-col items-center w-screen bg-neutral-100 bg-opacity-90">
+            <div>
+                <Image src="/character.png" alt="tft_logo" width={100} height={100} />
             </div>
-            <div className="ml-auto">
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-                <SignedOut>
-                    <SignInButton>
-                        <Button>Sign In</Button>
-                    </SignInButton>
-                </SignedOut>
+            <div className="flex flex-row justify-between w-full">
+                <div className="flex-grow text-center">
+                    <h1 className="text-4xl font-bold text-blue-900">TFT Leaderboard</h1>
+                </div>
+                <div className="ml-auto justify-center">
+                    <SignedIn>
+                        <div className="p-4">
+                            <UserButton />
+                        </div>
+                    </SignedIn>
+                    <SignedOut>
+                        <div className="p-4">
+                            <SignInButton>
+                                <Button>Sign In</Button>
+                            </SignInButton>
+                        </div>
+                    </SignedOut>
+                </div>
             </div>
         </header>
     )
