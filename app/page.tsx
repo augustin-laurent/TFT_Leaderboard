0a476ab35b1a1cfbd5ef2ac38c11a357ceffebd9
Augustin Laurent <augustin.lrt2@icloud.com>
2024-08-29 14:47:58 +0200

    Add sorting based on lp and rank to fix the case where two players are the same rank.

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@
 
 import { useEffect, useState } from "react";
 import { SignedIn } from "@clerk/nextjs";
+import axios from "axios";
 
 import { IPlayer } from "@/models/player";
 
-import axios from "axios";
+import { Columns } from "@/app/columns/column";
 
 import { DataTable } from "@/components/DataTable";
 import { PlayerInput } from "@/components/PlayerInput";
 import { Header } from "@/components/Header";
-
-import { Columns } from "@/app/columns/column";
 import { Footer } from "@/components/Footer";
 import { UpdateButton } from "@/components/UpdateButton";
+
 
 export default function Home() {
   const [players, setPlayers] = useState<IPlayer[]>([]);
